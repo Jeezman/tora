16213c0aa4f5762949b9927ec57a97ea66e3eb5b
Jeezman <tobiadeyemi91@gmail.com>
2022-05-09 14:54:19 +0100

    add password

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,21 +4,34 @@ import styles from '../styles/Login.module.css';
 export default function FirstPost() {
   return (
     <section className={styles.container}>
-          <aside className={styles.content}>
-              <h3 className={styles.copy}>Login and we'll automatically create an account 👍🏾</h3>
+      <aside className={styles.content}>
+        <h3 className={styles.copy}>
+          Login and we'll automatically create an account 👍🏾
+        </h3>
         <button className={`${styles.btn} ${styles.btn_primary}`}>
           <LightningIcon fill="#fff" className={styles.ln_icon} /> Login via
           Lightning
         </button>
         <p>or</p>
         <form>
-          <label className={styles.input_label} htmlFor="emailInput">Email</label>
+          <label className={styles.input_label} htmlFor="emailInput">
+            Email
+          </label>
           <input
             id="emailInput"
             aria-label="email"
             className={styles.input}
             type="email"
             placeholder="email@example.com"
+          />
+          <label className={styles.input_label} htmlFor="pwdInput">
+            Password
+          </label>
+          <input
+            id="pwdInput"
+            aria-label="pwd"
+            className={styles.input}
+            type="password"
           />
           <button id="email" className={`${styles.btn} ${styles.btn_primary}`}>
             Login via Email
