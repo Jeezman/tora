6ef47e88eee08cc9ee57fda6acb689b2b7121e1e
Jeezman <tobiadeyemi91@gmail.com>
2022-05-11 15:01:38 +0100

    add storage util

---
+++ client/util/storage.ts
@@ -0,0 +1,41 @@
+const storeData = async (key: string, value: string) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await window.localStorage.setItem(`@${key}`, jsonValue);
+    console.log('AsyncStorage set success')
+  } catch (e) {
+    console.log('AsyncStorage set error ', e);
+  }
+};
+
+const getData = async (key: string) => {
+  try {
+      const jsonValue = await window.localStorage.getItem(`@${key}`);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.log('AsyncStorage get error ', e);
+  }
+};
+
+const removeData = async (key: string) => {
+  try {
+    await window.localStorage.removeItem(`@${key}`)
+  } catch(e) {
+    console.log('AsyncStorage removeData error ', e);
+  }
+
+  console.log('Done.')
+}
+
+const clearAll = async () => {
+  try {
+    await window.localStorage.clear()
+  } catch(e) {
+    console.log('AsyncStorage clearAll error ', e);
+  }
+
+  console.log('Done.')
+}
+
+
+export { storeData, getData, removeData };
