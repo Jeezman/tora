3a0f1dee177e41eeee7483d6d11b9014a98d6626
elraphty <elraphtyofficial@gmail.com>
2022-05-16 00:29:06 +0100

    added bitcoin queries

---
+++ server/bitcoinqueries/Base.ts
@@ -0,0 +1,62 @@
+
+
+import Addresses from './Address';
+import Auth from './Auth';
+import Transactions from './Transaction';
+import { AxiosResponse } from 'axios';
+import { addressType } from '../interfaces/Address';
+import Wallet from './Wallet';
+
+export default class QueryBase extends Auth {
+    addresses: Addresses;
+    transactions: Transactions;
+    address: Addresses;
+    wallet: Wallet;
+
+    constructor(user: string | undefined, pass: string | undefined, rpcurl: string | undefined, port: number | undefined) {
+        super(user, pass, rpcurl, port);
+        this.addresses = new Addresses(this.user, this.pass, this.rpcurl, this.port);
+        this.transactions = new Transactions(this.user, this.pass, this.rpcurl, this.port);
+        this.address = new Addresses(this.user, this.pass, this.rpcurl, this.port);
+        this.wallet = new Wallet(this.user, this.pass, this.rpcurl, this.port);
+    }
+
+    /** Address Queries */
+    getNewAddress(label: string, type: addressType, wallet: string): Promise<AxiosResponse> {
+        return this.address.getNewAddress(label, type, wallet);
+    }
+    /** End Of Address Queries */
+
+    /** Transaction Queries */
+    getTransaction(txId: string, wallet: string): Promise<AxiosResponse> {
+        return this.transactions.getTransaction(txId, wallet);
+    }
+    decodeRawTransaction(transactionHex: symbol): Promise<AxiosResponse> {
+        return this.transactions.decodeRawTransaction(transactionHex);
+    }
+    createTransaction(wallet: string, address: string, amount: number, feerate: number = 1): Promise<AxiosResponse> {
+        return this.transactions.createTransaction(wallet, address, Number(amount.toFixed(8)), feerate);
+    }
+    getFeeEstimate(target: number): Promise<AxiosResponse> {
+        return this.transactions.getFeeEstimate(target);
+    }
+    setTransactionFee(wallet: string, txfee: number): Promise<AxiosResponse> {
+        return this.transactions.setTransactionFee(wallet, txfee);
+    }
+    /** End Of Transaction Queries */
+
+    /** Wallet Queries */
+    createWallet(name: string): Promise<AxiosResponse> {
+        return this.wallet.createWallet(name);
+    }
+    listWallets(): Promise<AxiosResponse> {
+        return this.wallet.listWallets();
+    }
+    getWalletBalance(name: string): Promise<AxiosResponse> {
+        return this.wallet.getWalletBalance(name);
+    }
+    getTransactions(name: string): Promise<AxiosResponse> {
+        return this.wallet.getTransactions(name);
+    }
+    /** End Of Wallet Queries */
+}
---
+++ server/bitcoinqueries/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import QueryBase from './Base';
+
+const USER = process?.env.BITCOIN_RPC_USER;
+const PASS = process?.env.BITCOIN_RPC_PASS;
+const RPCPORT = process?.env.BITCOIN_RPC_PORT;
+const RPCURL = process?.env.BITCOIN_RPC_URL;
+
+
+const base: QueryBase = new QueryBase(USER, PASS, RPCURL, Number(RPCPORT));
+
+export default base;
+
