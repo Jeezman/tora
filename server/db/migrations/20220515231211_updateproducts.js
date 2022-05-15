ba789d19666235efa2e33984bb3ab3beca0f719c
elraphty <elraphtyofficial@gmail.com>
2022-05-16 00:38:43 +0100

    finished bitcoin and lightning setup

--- server/db/migrations/20220515231211_updateproducts.js
+++ server/db/migrations/20220515231211_updateproducts.js
@@ -2,14 +2,16 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.alterTable('Products', (t) => {
+    t.string('image');
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('Products');
 };
