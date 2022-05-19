a488b0d886b32fac9528cbe4f8849491a6759876
Jeezman <tobiadeyemi91@gmail.com>
2022-05-19 11:05:12 +0100

    Update store endpoints - add store context

--- client/api/store.ts
+++ client/api/store.ts
@@ -1,21 +1,23 @@
 import axios, { AxiosError } from 'axios';
-import { ConsolidatedStoreDetail, StoreRequestModel } from '../pages/models/store.model';
+import { BASE_URL } from '.';
+import {
+  ConsolidatedProductDetail,
+  ConsolidatedStoreDetail,
+  ProductRequestModel,
+  StoreRequestModel,
+} from '../pages/models/store.model';
 
 let token;
-if (typeof window !== "undefined") {
-  token = window?.localStorage.getItem('@token')
+if (typeof window !== 'undefined') {
+  token = window?.localStorage.getItem('@token');
 
   if (token) {
-    token = JSON.parse(token)
+    token = JSON.parse(token);
   }
 }
-
-
-export const BASE_URL = 'http://localhost:5002/api/';
-
 axios.defaults.baseURL = BASE_URL;
 
-axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
 type ServerError = {
   error: string;
@@ -23,16 +25,17 @@ type ServerError = {
   data: any;
 };
 
-
-export const createStore = async (params: StoreRequestModel): Promise<ConsolidatedStoreDetail> => {
-    console.log(params)
+export const createStore = async (
+  params: StoreRequestModel
+): Promise<ConsolidatedStoreDetail> => {
+  console.log(params);
   try {
-      const res:ConsolidatedStoreDetail = await axios.post('store/create', {
-          name: params.name,
-      });
+    const res: ConsolidatedStoreDetail = await axios.post('store/create', {
+      name: params.name,
+    });
     return res;
   } catch (error: any) {
-    console.log('error is  ', error)
+    console.log('error is  ', error);
     if (axios.isAxiosError(error)) {
       const serverError = error as AxiosError<ServerError>;
       if (serverError && serverError.response) {
@@ -40,6 +43,43 @@ export const createStore = async (params: StoreRequestModel): Promise<Consolidat
       }
     }
     console.log('calling error ', error);
-    return  error.response.data;
+    return error.response.data;
   }
 };
+
+export const createProduct = async (
+  params: ProductRequestModel
+): Promise<ConsolidatedProductDetail> => {
+  console.log('createProduct ', params);
+  try {
+    const res:ConsolidatedProductDetail = await axios.post('store/product', params);
+    return res;
+  } catch (error: any) {
+    console.log('error is  ', error);
+    if (axios.isAxiosError(error)) {
+      const serverError = error as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        return serverError.response.data;
+      }
+    }
+    console.log('calling error ', error);
+    return error.response.data;
+  }
+};
+
+export const fetchProducts = async () => {
+  try {
+    const res = await axios.get(`store/products/taro?currentPage=0&perPage=10`);
+    return res.data;
+  } catch (error: any) {
+    console.log('error is  ', error);
+    if (axios.isAxiosError(error)) {
+      const serverError = error as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        return serverError.response.data;
+      }
+    }
+    console.log('calling error ', error);
+    return error.response.data;
+  }
+}
