03c82627cd0e74b2ac604afc934b4c9aec84713a
elraphty <elraphtyofficial@gmail.com>
2022-05-19 18:57:35 +0100

    added wallet migration

---
+++ server/db/migrations/20220519141721_transaction_logs.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('Transactions', (t) => {
+    t.increments('id').primary().notNullable();
+    t.integer('userId')
+      .notNullable()
+      .references('userId')
+      .inTable('Users');
+    t.integer('amount').notNullable();
+    t.text('transaction').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('Transactions');
+};
