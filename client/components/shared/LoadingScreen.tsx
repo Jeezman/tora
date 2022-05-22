02827a5c699453d925a9e977b8d562014585f5c1
Jeezman <tobiadeyemi91@gmail.com>
2022-05-22 23:16:59 +0100

    Add loading screen

---
+++ client/components/shared/LoadingScreen.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router';
+import { useContext, useEffect } from 'react';
+import { SpinnerIcon } from '../../assets/icons';
+import { AuthContext } from '../../pages/context/AuthContext';
+import { getData } from '../../util/storage';
+
+const LoadingScreen = () => {
+  const router = useRouter();
+  const { isLoggedIn } = useContext(AuthContext);
+  useEffect(() => {
+    const _getData = async () => {
+      let token = await getData('token');
+      console.log('token dey? ', token);
+      if (!token && !isLoggedIn) {
+        router.push('/');
+      }
+    };
+    _getData();
+  }, [isLoggedIn, router]);
+  return null;
+  return (
+    <div>
+      <SpinnerIcon size={40} color="#444" />
+    </div>
+  );
+};
+
+export default LoadingScreen;
